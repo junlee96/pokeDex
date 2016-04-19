ae560aaa5567b9a3cd6df06d5d830efe221f2aae
Wyman <wymanleejun@gmail.com>
2016-04-19 16:51:44 +0800

    Initial Commit

---
+++ pokeDexTests/pokeDexTests.swift
@@ -0,0 +1,36 @@
+//
+//  pokeDexTests.swift
+//  pokeDexTests
+//
+//  Created by Admin on 19/4/16.
+//  Copyright © 2016 Dreamsmart. All rights reserved.
+//
+
+import XCTest
+@testable import pokeDex
+
+class pokeDexTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
