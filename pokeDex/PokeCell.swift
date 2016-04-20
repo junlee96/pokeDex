0eac873f5e21e221c209858b21bf94e23c755a64
Wyman <wymanleejun@gmail.com>
2016-04-20 13:03:14 +0800

    Attached actions to label and thumbnail

---
+++ pokeDex/PokeCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokeCell.swift
+//  pokeDex
+//
+//  Created by Admin on 20/4/16.
+//  Copyright © 2016 Dreamsmart. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+
+}
